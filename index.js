daf6ea75f7cc2c0702d8a39a7bb9d9cc0c394f9b
professionalcoding400-ui <professionalcoding400@gmail.com>
2025-09-03 20:12:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.send('Hello From the Backend Deployment !');
     });
 
